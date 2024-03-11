67814d1e9cb40f6efe69a6ef1c83339dc306e7dc
Nicholas Schoen <scheontransit@gmail.com>
2024-03-11 11:07:46 -0500

    adding a commit for more practice with git-hub

--- change.js
+++ change.js
@@ -5,3 +5,7 @@ if (missouriManeCoons === cool) {
     alert("Missouri Maine Coons is the Best Cattery Ever!");
     return;
 }
+
+/* this is a comment to show changes to get practice with working with git-hub
+do not mind this code at all
+*/
